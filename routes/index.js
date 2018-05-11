c145116040a9e1a71f867e10039b5f7fa1386232
Armen <danielyan_armen@yahoo.com>
2018-05-11 23:46:12 +0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express'),
+  router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
